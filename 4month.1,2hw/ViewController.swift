8fc8aaee6c8716c3044c9c5b973eecb586873cbd
imrannn24 <imran200424@icloud.com>
2023-06-26 02:41:05 +0600

    Initial Commit

---
+++ 4month.1,2hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4month.1,2hw
+//
+//  Created by imran on 26.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
